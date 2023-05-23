22624be48bd237d8c2ce7efd388f841e38f57c85
Dragesia <brkgzlsy@gmail.com>
2023-05-23 17:33:36 +0300

    bugfix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ character, onSelect }) {
     return (
-        <div className="card" onClick={onSelect}>
-            <img src={character.src} alt={character.name} />
+        <div className="card">
+            <img src={character.src} alt={character.name} onClick={onSelect} />
             <div className="name">{character.name}</div>
         </div>
     )
